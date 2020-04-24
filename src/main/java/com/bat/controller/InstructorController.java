124d238abb37025cfcd285a0e3d44fb6bbe9c563
OmarTheDarkKnight <jayed258@gmail.com>
2020-04-25 04:21:16 +0600

    Add string trimmer for all incoming form data in InstructorController

--- src/main/java/com/bat/controller/InstructorController.java
+++ src/main/java/com/bat/controller/InstructorController.java
@@ -1,10 +1,15 @@
 package com.bat.controller;
 
 import com.bat.model.Instructor;
+import com.bat.model.InstructorDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bat.alfred.Helper;
@@ -21,6 +26,17 @@ public class InstructorController {
 	
 	@Autowired
 	private Helper helper;
+
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		// Spring class for string trim
+		// "true" in the constructor means it will trim the spaces down to null
+		// if there's only white spaces in the data filed
+		StringTrimmerEditor stringTrimmer = new StringTrimmerEditor(true);
+
+		// registering it as a custom editor that will work on every String class as specified
+		webDataBinder.registerCustomEditor(String.class, stringTrimmer);
+	}
 	
 	@GetMapping("/all")
 	public String getInstructorList(Model model) {
