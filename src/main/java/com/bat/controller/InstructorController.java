7e69a09379b10b984d7c951e9d58cb31b8839115
OmarTheDarkKnight <jayed258@gmail.com>
2020-05-04 04:42:25 +0600

    Add deleteInstructor method for 'delete' url

--- src/main/java/com/bat/controller/InstructorController.java
+++ src/main/java/com/bat/controller/InstructorController.java
@@ -13,9 +13,12 @@ import org.springframework.web.bind.annotation.*;
 
 import com.bat.alfred.Helper;
 import com.bat.service.InstructorService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/instructor")
@@ -48,11 +51,11 @@ public class InstructorController {
 	}
 
 	@GetMapping("/instructor-form")
-	public String showInstructorForm(@RequestParam("target") String instructorId, Model model) {
-		if(!StringUtils.isEmpty(instructorId)) {
+	public String showInstructorForm(@RequestParam("target") String theId, Model model) {
+		if(!StringUtils.isEmpty(theId)) {
 			try{
-				int theId = Integer.parseInt(instructorId);
-				Instructor instructor = instructorService.getById(theId);
+				int instructorId = Integer.parseInt(theId);
+				Instructor instructor = instructorService.getById(instructorId);
 				model.addAttribute("instructor", instructor);
 			} catch (Exception exception) {
 				return "redirect:/instructor/all";
@@ -76,4 +79,19 @@ public class InstructorController {
 		instructorService.save(theInstructor);
 		return "redirect:/instructor/all";
 	}
+
+	@PostMapping("/delete")
+    public String deleteInstructor(@RequestParam("theId") String theId, Model model, RedirectAttributes redirectAttr) {
+        Map<String, String> messages = new HashMap<>();
+        try{
+            instructorService.delete(theId);
+        } catch (Exception exception) {
+            messages.put("error", "Could not delete resource");
+            redirectAttr.addFlashAttribute("messages", messages);
+            return "redirect:/instructor/all";
+        }
+        messages.put("success", "Instructor deleted successfully");
+        redirectAttr.addFlashAttribute("messages", messages);
+	    return "redirect:/instructor/all";
+    }
 }
