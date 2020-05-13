c0a0abb34c96793b75622888104cc1ea86c83485
OmarTheDarkKnight <jayed258@gmail.com>
2020-05-13 09:40:56 +0600

    Add transient vairiable rating in Course model

--- src/main/java/com/bat/model/Course.java
+++ src/main/java/com/bat/model/Course.java
@@ -3,16 +3,7 @@ package com.bat.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="course")
@@ -24,6 +15,9 @@ public class Course {
 	
 	@Column(name="title")
 	private String title;
+
+	@Transient
+	private float rating;
 	
 	@ManyToOne(cascade= {CascadeType.DETACH, CascadeType.MERGE,
 						CascadeType.PERSIST, CascadeType.REFRESH})
@@ -54,6 +48,14 @@ public class Course {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public float getRating() {
+		return rating;
+	}
+
+	public void setRating(float rating) {
+		this.rating = rating;
 	}
 
 	public Instructor getInstructor() {
