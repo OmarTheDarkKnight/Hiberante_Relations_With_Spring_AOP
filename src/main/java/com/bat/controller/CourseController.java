a8eca24906cfecc78e339353ba6623cfa77a9811
OmarTheDarkKnight <jayed258@gmail.com>
2020-05-15 14:19:34 +0600

    Add saveCourse method for add or update courses

--- src/main/java/com/bat/controller/CourseController.java
+++ src/main/java/com/bat/controller/CourseController.java
@@ -9,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -74,6 +74,16 @@ public class CourseController {
             return "redirect:/course/all";
         }
         return alfred.buildViewName(folderName, "courseForm");
+    }
+
+    @PostMapping("/saveCourse")
+    public String saveCourse(@Valid @ModelAttribute("course") Course theCourse,
+                             BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return alfred.buildViewName(folderName, "courseForm");
+        }
+        courseService.save(theCourse);
+        return "redirect:/course/all";
     }
 }
 
