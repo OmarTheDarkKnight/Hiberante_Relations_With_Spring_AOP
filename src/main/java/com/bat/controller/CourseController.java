2c5cec9841792c5afca4dfb94be9d86c2672c90c
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-12 05:35:39 +0600

    Add @InitBinder to trim off the whitespaces of input in CourseController

--- src/main/java/com/bat/controller/CourseController.java
+++ src/main/java/com/bat/controller/CourseController.java
@@ -6,10 +6,12 @@ import com.bat.model.Instructor;
 import com.bat.service.CourseService;
 import com.bat.service.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -31,6 +33,17 @@ public class CourseController {
     private Helper alfred;
 
     private String folderName = "course";
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        // Spring class for string trim
+        // "true" in the constructor means it will trim the spaces down to null
+        // if there's only white spaces in the data filed
+        StringTrimmerEditor stringTrimmer = new StringTrimmerEditor(true);
+
+        // registering it as a custom editor that will work on every String class as specified
+        webDataBinder.registerCustomEditor(String.class, stringTrimmer);
+    }
 
     @GetMapping("/all")
     public String courseList(Model model) {
