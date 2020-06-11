8fb34029c08ac7bb3a8f501993cd0229ac558c17
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-12 04:56:57 +0600

    Add course dto

---
+++ src/main/java/com/bat/dto/CourseDto.java
@@ -0,0 +1,42 @@
+package com.bat.dto;
+
+import java.io.Serializable;
+
+public class CourseDto extends BaseDto implements Serializable {
+    private int id;
+    private String encId;
+    private String title;
+    private float rating;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEncId() {
+        return encId;
+    }
+
+    public void setEncId(String encId) {
+        this.encId = encId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+}
