d3a168c188098104f897f92e2a02d5328efe489c
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-22 06:34:37 +0600

    Add ReviewController with showReviewsOfACourse method

---
+++ src/main/java/com/bat/controller/ReviewController.java
@@ -0,0 +1,39 @@
+package com.bat.controller;
+
+import com.bat.alfred.Helper;
+import com.bat.service.interfaces.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/reviews")
+public class ReviewController {
+    @Autowired
+    private Helper alfred;
+
+    @Autowired
+    private ReviewService reviewService;
+
+    private String folderName = "reviews";
+
+    @GetMapping("/c/{courseId}")
+    public String showReviewsOfACourse(@PathVariable("courseId") String courseId, Model model, RedirectAttributes redirectAttr) {
+        try {
+            model.addAttribute("course", reviewService.getReviewsOfACourse(courseId));
+        } catch (Exception exception) {
+            Map<String, String> messages = new HashMap<String, String>();
+            messages.put("error", "Something went wrong. Please try again.");
+            redirectAttr.addFlashAttribute("messages", messages);
+            return "redirect:/course/all";
+        }
+        return alfred.buildViewName(folderName, "reviews_of_course");
+    }
+}
