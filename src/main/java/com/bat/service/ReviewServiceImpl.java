a8fc14bbaf04c147c068da9ec319d7a9140fce19
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-22 07:52:44 +0600

    Add getReviewsOfACourse method in review service

--- src/main/java/com/bat/service/ReviewServiceImpl.java
+++ src/main/java/com/bat/service/ReviewServiceImpl.java
@@ -1,18 +1,17 @@
 package com.bat.service;
 
-import com.bat.dao.ReviewDao;
+import com.bat.dto.CourseDto;
+import com.bat.dto.ReviewDto;
 import com.bat.model.Review;
 import com.bat.service.interfaces.ReviewService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
-public class ReviewServiceImpl implements ReviewService {
-    @Autowired
-    private ReviewDao reviewDao;
-
+public class ReviewServiceImpl extends BaseService implements ReviewService {
     @Override
     public void save(Review newReview) {
 //        reviewDao.save(newReview);
@@ -22,6 +21,19 @@ public class ReviewServiceImpl implements ReviewService {
     public Review getReviewById(String theId) {
         int reviewId = Integer.parseInt(theId);
         return reviewDao.getById(reviewId);
+    }
+
+    @Override
+    public CourseDto getReviewsOfACourse(String courseId) {
+        CourseDto courseDto = courseDao.getCourseByIdWithInstructor(decrypt(courseId, courseSalt));
+        courseDto.setRating(reviewDao.getAvgRatingByCourse(courseDto.getId()));
+
+        List<ReviewDto> reviewDtoList = reviewDao.getByCourse(courseDto.getId());
+        reviewDtoList.forEach(review-> {
+            review.setEncId(encrypt(review.getId(), reviewWSalt));
+        });
+        courseDto.setReviewDtoList(reviewDtoList);
+        return courseDto;
     }
 
     @Override
