4afe78f4a3f4243a5c94c5ebd9dbe137d5ef8246
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-27 08:48:13 +0600

    Add @Unique validation annotation

---
+++ src/main/java/com/bat/annotations/Unique.java
@@ -0,0 +1,26 @@
+package com.bat.annotations;
+
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target( { ElementType.FIELD } )
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Unique {
+    // define default table name
+    public String table();
+
+    // define default column name
+    public String column();
+
+    // define default error message
+    public String message() default "Value already exists.";
+
+    // define default groups
+    public Class<?>[] groups() default {};
+
+    // define default payloads
+    public Class<? extends Payload>[] payload() default {};
+}
