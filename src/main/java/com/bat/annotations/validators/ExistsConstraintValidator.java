8b8485094c3ec6afd6dfafa279a991146db3a0aa
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-27 08:43:19 +0600

    Add ExistsConstraintValidator to validate the value passed to the annotation

---
+++ src/main/java/com/bat/annotations/validators/ExistsConstraintValidator.java
@@ -0,0 +1,27 @@
+package com.bat.annotations.validators;
+
+import com.bat.annotations.Exists;
+import com.bat.service.interfaces.AnnotationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ExistsConstraintValidator implements ConstraintValidator<Exists, String> {
+    private String tableName;
+    private String columnName;
+
+    @Autowired
+    AnnotationService annotationService;
+
+    @Override
+    public void initialize(Exists constraintAnnotation) {
+        this.tableName = constraintAnnotation.table();
+        this.columnName = constraintAnnotation.column();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        return value != null && annotationService.exists(tableName, columnName, value);
+    }
+}
