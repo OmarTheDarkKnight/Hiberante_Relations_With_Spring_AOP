bbd5866f37801eb02e5c931aa15f49795da29ad6
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-27 08:44:16 +0600

    Add @Constraint in exists annotation to define the validator

--- src/main/java/com/bat/annotations/Exists.java
+++ src/main/java/com/bat/annotations/Exists.java
@@ -1,11 +1,15 @@
 package com.bat.annotations;
 
+import com.bat.annotations.validators.ExistsConstraintValidator;
+
+import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Constraint(validatedBy = ExistsConstraintValidator.class)
 @Target( { ElementType.FIELD } )
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Exists {
