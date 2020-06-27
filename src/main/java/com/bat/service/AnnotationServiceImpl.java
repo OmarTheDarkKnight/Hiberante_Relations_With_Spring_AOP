020c226024cc219acb36c3aee7c012609b0affaf
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-27 08:40:45 +0600

    Add Annotation Service and Implementation along with exists method

---
+++ src/main/java/com/bat/service/AnnotationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bat.service;
+
+import com.bat.dao.AnnotationDao;
+import com.bat.service.interfaces.AnnotationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class AnnotationServiceImpl implements AnnotationService {
+    @Autowired
+    AnnotationDao annotationDao;
+
+    @Override
+    public boolean exists(String table, String column, String value) {
+        return false;
+    }
+}
---
+++ src/main/java/com/bat/service/interfaces/AnnotationService.java
@@ -0,0 +1,5 @@
+package com.bat.service.interfaces;
+
+public interface AnnotationService {
+    boolean exists(String table, String column, String value);
+}
