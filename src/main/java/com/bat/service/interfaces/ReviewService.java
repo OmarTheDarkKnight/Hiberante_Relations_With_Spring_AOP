8f85f285895fe3ebf1346ed9fe49fb21c8cb5ea6
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-27 09:08:29 +0600

    Update ReviewService
    Add getReview method and Remove getReviewById method

--- src/main/java/com/bat/service/interfaces/ReviewService.java
+++ src/main/java/com/bat/service/interfaces/ReviewService.java
@@ -1,11 +1,12 @@
 package com.bat.service.interfaces;
 
 import com.bat.dto.CourseDto;
+import com.bat.dto.ReviewDto;
 import com.bat.model.Review;
 
 public interface ReviewService {
     void save(Review newReview);
-    Review getReviewById(String theId);
     CourseDto getReviewsOfACourse(String courseId);
+    ReviewDto getReview(String theEncReviewId);
     void delete(String theId);
 }
