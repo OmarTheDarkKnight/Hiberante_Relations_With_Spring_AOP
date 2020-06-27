60f6e9ce4500c7a3564a112cae8f5ba179b876e9
OmarTheDarkKnight <jayed258@gmail.com>
2020-06-27 08:39:28 +0600

    Add occurrenceOfAValue method for counting the occurrence number of a value in a certain column in a certain table all passed in the parameter

--- src/main/java/com/bat/dao/AnnotationDao.java
+++ src/main/java/com/bat/dao/AnnotationDao.java
@@ -2,6 +2,14 @@ package com.bat.dao;
 
 import org.springframework.stereotype.Repository;
 
+import java.math.BigInteger;
+
 @Repository
 public class AnnotationDao extends HelperDao {
+    public int occurrenceOfAValue(String table, String column, String value) {
+        BigInteger result = (BigInteger) hibernateQuery("SELECT COUNT(*) FROM "+ table +" WHERE "+ column +"=:value")
+                .setParameter("value", value)
+                .getSingleResult();
+        return result.intValue();
+    }
 }
