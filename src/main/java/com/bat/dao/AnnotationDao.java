53318b7ce0a5b81ae1c33bfc9d4f522fd383b180
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-05 00:43:14 -0400

    Add exceptId in occurrenceOfAValue method's parameter and Update body accordingly to check the occurrence of a value except a certain id

--- src/main/java/com/bat/dao/AnnotationDao.java
+++ src/main/java/com/bat/dao/AnnotationDao.java
@@ -6,10 +6,18 @@ import java.math.BigInteger;
 
 @Repository
 public class AnnotationDao extends HelperDao {
-    public int occurrenceOfAValue(String table, String column, String value) {
-        BigInteger result = (BigInteger) hibernateQuery("SELECT COUNT(*) FROM "+ table +" WHERE "+ column +"=:value")
-                .setParameter("value", value)
-                .getSingleResult();
+    public int occurrenceOfAValue(String table, String column, String value, int exceptId) {
+        BigInteger result = null;
+        if(exceptId == 0) {
+            result = (BigInteger) hibernateQuery("SELECT COUNT(*) FROM "+ table +" WHERE "+ column +"=:value")
+                    .setParameter("value", value)
+                    .getSingleResult();
+        } else {
+            result = (BigInteger) hibernateQuery("SELECT COUNT(*) FROM "+ table +" WHERE "+ column +"=:value AND id!=:exceptId")
+                    .setParameter("value", value)
+                    .setParameter("exceptId", exceptId)
+                    .getSingleResult();
+        }
         return result.intValue();
     }
 }
