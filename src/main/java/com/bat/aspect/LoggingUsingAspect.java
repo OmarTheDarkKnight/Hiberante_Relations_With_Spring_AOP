0dbc06883042cd3516b8322189bec41f118c2040
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-05 23:38:13 -0400

    Add @Pointcut declarations

--- src/main/java/com/bat/aspect/LoggingUsingAspect.java
+++ src/main/java/com/bat/aspect/LoggingUsingAspect.java
@@ -6,4 +6,19 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingUsingAspect {
+    // setup pointcut declarations
+    @Pointcut("execution(* com.bat.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    @Pointcut("execution(* com.bat.service.*.*(..))")
+    private void forServicePackage() {}
+
+    @Pointcut("execution(* com.bat.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Pointcut("execution(* com.bat.dto.*.*(..))")
+    private void forDtoPackage() {}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage() || forDtoPackage()")
+    private void forAppFlow() {}
 }
