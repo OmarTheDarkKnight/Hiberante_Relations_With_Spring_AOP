19f53921b7bca7cdb7b85f04e40ffe72e424ce16
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-05 23:39:06 -0400

    Add @Before advice

--- src/main/java/com/bat/aspect/LoggingUsingAspect.java
+++ src/main/java/com/bat/aspect/LoggingUsingAspect.java
@@ -1,11 +1,17 @@
 package com.bat.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 @Aspect
 @Component
 public class LoggingUsingAspect {
+    // setup logger
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
     // setup pointcut declarations
     @Pointcut("execution(* com.bat.controller.*.*(..))")
     private void forControllerPackage() {}
@@ -21,4 +27,23 @@ public class LoggingUsingAspect {
 
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage() || forDtoPackage()")
     private void forAppFlow() {}
+
+    // add @Before advice
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint) {
+        // display method we are calling
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====>> in @Before: calling method: " + theMethod);
+
+        // display the arguments to the method
+
+        // get the arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru and display args
+        for (Object tempArg : args) {
+            myLogger.info("=====>> argument: " + tempArg);
+        }
+
+    }
 }
